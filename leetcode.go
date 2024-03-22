d871c651cd8bc507a342c4d89a5a4956b858b8f2
Benjamin Roberts <roberts.b.d@pm.me>
2024-03-22 21:29:59 +0000

    Update leetcode.go

--- leetcode.go
+++ leetcode.go
@@ -66,3 +66,71 @@ func longestCommonPrefix(strs []string) string {
     }
 return strs[0][0:ch]
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type List struct {
+    head *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    
+    num1:= 0;
+    num2:= 0;
+    i := 0;
+    curNode := l1;
+
+    for {
+        num1 += curNode.Val*int(math.Pow10(i));
+        if curNode.Next == nil {
+            break;
+        } else {
+            curNode = curNode.Next; 
+            i++;
+        }
+    }
+
+    fmt.Println(num1)
+
+    curNode = l2;
+    i = 0;
+
+    for {
+        num2 += curNode.Val*int(math.Pow10(i));
+        if curNode.Next == nil {
+            break;
+        } else {
+            curNode = curNode.Next; 
+            i++;
+        }
+    }
+
+    fmt.Println(num2)
+
+    outNum := strconv.Itoa(num1 + num2);
+
+    outList := List{};
+
+    for i := len(outNum) - 1; i >= 0; i-- {
+       node := &ListNode{
+           Val: int(outNum[i] - '0'),
+       } 
+       if outList.head == nil {
+            outList.head = node; 
+        } else {
+            c := outList.head;
+            for c.Next != nil {
+                c = c.Next;
+            }
+            c.Next = node;
+        }
+   }
+
+    return outList.head; 
+}
